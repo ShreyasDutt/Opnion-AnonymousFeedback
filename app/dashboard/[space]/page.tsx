a7a5dadd5c4b38acc0032a6d01593f4274f87c3c
Shreyas Dutt <151562739+ShreyasDutt@users.noreply.github.com>
2025-07-01 20:41:18 +0530

    lint errors

--- app/dashboard/[space]/page.tsx
+++ app/dashboard/[space]/page.tsx
@@ -1,46 +1,46 @@
+import { Metadata } from 'next'
+import { notFound } from 'next/navigation'
+import { ChartLine, Pencil, Settings } from 'lucide-react'
 import Navbar from '@/components/Navbar'
-import React from 'react'
+import FeedbackUi from '@/components/FeedbackUi'
 import LogoPng from '@/public/Opnion.png'
 import Image from 'next/image'
 import { Button } from '@/components/ui/button'
-import { ChartLine, Pencil, Settings } from 'lucide-react'
-import FeedbackUi from '@/components/FeedbackUi'
 
-const page = async({params}:{params:{space:string}}) => {
+interface PageProps {
+  params: {
+    space: string
+  }
+}
+
+const Page = async ({ params }: PageProps) => {
   return (
     <div>
-        <Navbar/>
-        <div className='flex flex-col py-10 px-6 gap-3'>
-            <div className='flex gap-3 justify-between items-start'>
-                <div className='flex items-center gap-2'>
-                    <Image src={LogoPng} height={30} width={30} alt=''/>
-                    <p className='text-2xl font-bold'>{params.space}</p>
-                </div>
+      <Navbar />
+      <div className='flex flex-col py-10 px-6 gap-3'>
+        <div className='flex gap-3 justify-between items-start'>
+          <div className='flex items-center gap-2'>
+            <Image src={LogoPng} height={30} width={30} alt='' />
+            <p className='text-2xl font-bold'>{params.space}</p>
+          </div>
 
-                 <div className='flex gap-3 items-center'>
-                    <div className='flex items-center gap-3'>
-                    <Button variant={'outline'} size={'icon'} effect={'ringHover'}>
-                        <ChartLine />
-                    </Button>
-                        <Button effect="expandIcon" icon={Pencil} iconPlacement="right">
-                        Edit Space
-                        </Button>
-                    <Button variant={'outline'} size={'icon'} effect={'ringHover'}>
-                        <Settings />
-                    </Button>
-                    </div>
-                </div>
-            </div>
-
-            <FeedbackUi/>
-        
-        
-               
-
-
+          <div className='flex gap-3 items-center'>
+            <Button variant='outline' size='icon' effect='ringHover'>
+              <ChartLine />
+            </Button>
+            <Button effect='expandIcon' icon={Pencil} iconPlacement='right'>
+              Edit Space
+            </Button>
+            <Button variant='outline' size='icon' effect='ringHover'>
+              <Settings />
+            </Button>
+          </div>
         </div>
+
+        <FeedbackUi />
+      </div>
     </div>
   )
 }
 
-export default page
+export default Page
